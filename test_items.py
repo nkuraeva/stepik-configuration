704c9f1ff13974520da0af7cc7f0cdec675011f2
nkuraeva <57593042+nkuraeva@users.noreply.github.com>
2021-01-25 15:59:04 +0100

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_basket_button_on_the_page(browser):
     browser.get(link)
-    btn = browser.find_elements_by_css_selector(".btn-primary .btn-add-to-busket")
+    btn = browser.find_elements_by_css_selector(".btn-primary .btn-add-to-basket")
     assert btn != 0, "Button was not found"
